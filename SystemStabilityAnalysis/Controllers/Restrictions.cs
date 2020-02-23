7d34a0a369e842159bf48a4af000d0dbc4424051
GMIKE <roman.soloweow333@ya.ru>
2020-02-24 00:02:00 +0300

    fix format data from responce

--- SystemStabilityAnalysis/Controllers/Restrictions.cs
+++ SystemStabilityAnalysis/Controllers/Restrictions.cs
@@ -7,26 +7,27 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using SystemStabilityAnalysis.Helpers;
 using SystemStabilityAnalysis.Models;
-
+using System.Text.Json;
 namespace SystemStabilityAnalysis.Controllers
 {
     [ApiController]
     [Route("[controller]/[action]")]
+    [Produces("application/json")]
     public class Restrictions : ControllerBase
     {
         [HttpGet]
-        public ActionResult GetParameters()
+        public Dictionary<string, object> GetParameters()
         {
             Dictionary<string, object> Data = new Dictionary<string, object>();
             Data.Add("Status", Status.Success.GetName());
             Data.Add("Properties", StaticData.PropertiesSystem.Properties);
-            var result = JsonConvert.SerializeObject(Data);
-            return new JsonResult(result);
+            return Data;
         }
 
         [HttpGet]
         public string GetTest()
         {
+
             return "Здесь будет запрос для получения условий";
         }
     }
