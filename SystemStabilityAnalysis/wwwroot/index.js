6cb49d108379b9cd584484dc3f8fb8452e20e4a2
Dmitry Ryabokon <sangvinion1@gmail.com>
2020-02-24 15:24:27 +0300

    Filters, tabel

--- SystemStabilityAnalysis/wwwroot/index.js
+++ SystemStabilityAnalysis/wwwroot/index.js
@@ -16,22 +16,6 @@ $('.ui.form')
             type   : 'empty'
           }
         ]
-      },
-      skills: {
-        identifier  : 'skills',
-        rules: [
-          {
-            type   : 'empty'
-          }
-        ]
-      },
-      conditions: {
-        identifier  : 'conditions',
-        rules: [
-          {
-            type   : 'empty'
-          }
-        ]
       }
     }
   })
@@ -41,12 +25,17 @@ $('.message .close').on('click', function() {
   $(this).closest('.message').transition('fade');
 });
 
-$(".ui.icon.button.plus").submit(addFilter)
-$(".ui.icon.button.minus").click(deleteFilter)
-$(".ui.button.next").click(nextPage)
-$(".ui.button.delete-all").click(()=>{$(".ui.celled.table").remove()})
+$(".ui.icon.button.plus").click(function(eventData, handler) {
+  $( ".ui.form" ).submit();
+  if ($(".ui.form.success").length === 1)
+    addFilter();
+});
+$(".ui.icon.button.minus").click(deleteFilter);
+$(".ui.button.next").click(nextPage);
+$(".ui.button.delete-all").click(()=>{$(".ui.celled.table").remove()});
 $(".ui.dropdown.names").click(getNames);
 $(".ui.dropdown.conditions").click(getConditions);
+$(".ui.button.save-system").click(saveSystem);
 $('.ui.dropdown.names').change(function(){
   setTimeout(()=>{
     currentElement = $(".ui.dropdown.names").find(".item.active");
@@ -56,44 +45,25 @@ $('.ui.dropdown.names').change(function(){
 });
 
 
-function nextPage(){
-  currentTab = $('.active.item');
+function nextPage(event){
+  currentTab = $(event.target).closest('.segment').parent().find(".item.active");
   currentTab = currentTab.removeClass('active');
   nextTab = currentTab.next()
   currentTab = currentTab.next().tab('change tab', nextTab.attr('data-tab'))
 }
 
 function addFilter(){
-  let currentCombobox = $(".ui.dropdown.names");
-  currentCombobox.dropdown('clear')
-  let notificationMessage = ""
+  //let currentCombobox = $(".ui.dropdown.names");
+  let message = "Ограничение не добавлено.";
+  let parameter = $(".ui.names").find(".item.active").attr("data-value");
+  let condition = $(".ui.conditions ").find(".item.active").attr("data-value");
+  let value = $(".input.value ").val();
   $.ajax({
-    method: "POST",
-    url: "Restrictions/AddRestriction",
-    data: {
-      'parameter': '',
-      'condition': '',
-      'value': ''
-    }
+    method: "GET",
+    url: `Restrictions/AddRestriction?parameter=${parameter}&condition=${condition}&value=${value}`,
   }).done(function(msg){
-    console.log(msg)
     if (msg.status == "Success") {
-      if ($(".ui.celled.table").length > 0) {
-        $(".ui.celled.table tr:last").after(`<tr>
-        <td data-label="Name">Elyse</td>
-        <td data-label="Age">24</td>
-        <td data-label="Job">Designer</td>
-        <td data-label="Age">24</td>
-        <td data-label="Job">Engineer</td>
-        <td data-label="Job" class="center aligned" >
-          <button class="ui icon button minus">
-            <i class="minus icon"></i>
-          </button>
-        </td>
-        </tr>`)
-        
-      }
-      else {
+      if ($(".ui.celled.table").length == 0 ) {
         $('.ui.form').append(`<table class="ui celled blue table center aligned">
         <thead>
           <tr>
@@ -106,36 +76,44 @@ function addFilter(){
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td data-label="Name">James</td>
-            <td data-label="Age">24</td>
-            <td data-label="Job">Engineer</td>
-            <td data-label="Age">24</td>
-            <td data-label="Job">Engineer</td>
-            <td data-label="Job" class="center aligned" >
-              <button class="ui icon button minus">
-                <i class="minus icon"></i>
-              </button>
-            </td>
-          </tr>
         </tbody>
         </table>`)
       }
+      $(".ui.celled.table tbody").after(`<tr>
+      <td data-label="description">${msg.description}</td>
+      <td data-label="name">${msg.name}</td>
+      <td data-label="unit">${msg.unit}</td>
+      <td data-label="condition">${msg.condition}</td>
+      <td data-label="value">${msg.value}</td>
+      <td data-label="button" class="center aligned" >
+        <button class="ui icon button minus">
+          <i class="minus icon"></i>
+        </button>
+      </td>
+      </tr>`)
       $(".ui.icon.button.minus").unbind();
       $(".ui.icon.button.minus").click(deleteFilter);
     }
-    notification(msg.status,msg.Message,"first");
-    $(".ui.dropdown.names").remove();
-    $(".ui.dropdown.names").dropdown('clear');
+    else
+      notification(msg.status,msg.message,"first");
+    clearFilters();
     getNames();
   });
 }
 
 function deleteFilter(){
+  console.log($( this ).parent().parent().find("[data-label='name']"))
   $( this ).parent().parent().remove();
   if ($(".ui.celled.table tr").length == 1) {
     $(".ui.celled.table").remove();
   }
+  $.ajax({
+    method: "GET",
+    url: "Restrictions/DeleteRestriction?restrictionName=DeltaT",
+  }).done(function(msg){
+
+  });
+
 }
 
 function getNames(){
@@ -219,9 +197,24 @@ function notification(type, message, tabNum){
 }
 
 function clearFilters(){
+  $(".ui.dropdown.names").find(".menu").empty();
   $(".ui.dropdown.names").dropdown('clear');
   $(".ui.dropdown.conditions").dropdown('clear');
   $('.disField').find(".name").val('');
   $('.disField').find(".unit").val('');
-  $('.disField').find(".value").val('');
+  $('.input.value').val('');
+}
+
+function saveSystem(event){
+  if ($(".ui.input.save-system").length == 0) {
+    $(event.target).parent().append(`
+      <div class="ui input save-system">
+        <input type="text" placeholder="Имя системы">
+      </div>
+    `);
+    $(".ui.button.save-system").text("Сохранить");
+  }
+  else {
+    //сохранить
+  }
 }
